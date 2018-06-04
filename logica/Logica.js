74a35f840e2692002f8b177b327401f143a9c57c
osblasae <osblasae@epsg.upv.es>
2018-06-04 11:53:29 +0200

    Primera práctica (Lógica del Negocio) terminada

--- logica/Logica.js
+++ logica/Logica.js
@@ -15,89 +15,235 @@ class Logica {
 	// 
 	// nombreBD: Texto
 	// -->
-	//    f () 
+	//    constructor () 
 	// -->
 	// Terminado // via callback()
 	// 
 	// .................................................................
-	constructor( nombreBD, callback ) {
-		this.laConexion = new ConexionBD( nombreBD, callback )
+	constructor ( nombreBD, callback ) {
+		this.laConexion = new ConexionBD( nombreBD, callback ) ;
 	}
 
 	// .................................................................
 	// .................................................................
 	cerrar() {
-		this.laConexion.cerrar()
+		this.laConexion.cerrar() ;
 	}
 
 	// .................................................................
 	//
 	// datosZona: JSON {nombre: Texto, descripcion: Texto}
 	// -->
-	//    f () 
+	//    nuevaZona () 
 	// -->
 	// void / Error // via callback( err )
 	//
 	// .................................................................
-	nuevaZona( datosZona, callback ) {
+	nuevaZona ( datosZona, callback ) {
 
-		var textoSQL = 'insert into Zona values ( $nombre, $descripcion );'
+		var textoSQL = 'insert into Zona values ( $nombre, $descripcion );' ;
+
 		var datos = {
 			$nombre: datosZona.nombre,
 			$descripcion: datosZona.descripcion
-		}
+		} ;
 		
-		this.laConexion.modificarConPrepared( textoSQL, datos, callback )
+		this.laConexion.modificarConPrepared( textoSQL, datos, callback ) ;
 											  
-	} // ()
+	} // nuevaZona()
 
 	// .................................................................
 	// 
-	//    f()
+	//    borrarTodosLosDatos()
 	// -->
 	// Terminado // via callbak()
 	// 
 	// .................................................................
-	borrarTodosLosDatos( callback ) {
-		var self = this
+	borrarTodosLosDatos ( callback ) {
+
+		var self = this ;
 
 		//
 		// borrar primero los que dependen de otros (foreign key)
 		//
-		this.laConexion.modificar( "delete from Vertice;", function () {
-			self.laConexion.modificar( "delete from Zona;", function () {
-				callback()
-			})
-		})
-	} // ()
+
+		this.laConexion.modificar( "delete from Vertice;", () => {
+
+			self.laConexion.modificar( "delete from Zona;", () => {
+
+				callback() ;
+
+			});
+
+		});
+
+	} // borrarTodosLosDatos()
 
 	// .................................................................
 	//
 	// nombreZona: texto
 	// vertice: JSON {latitud: R, longitud: R}
 	// -->
-	//    f () 
+	//    nuevoVerticeParaZona() 
 	// -->
 	// void / Error // via callback( err )
 	//
 	// .................................................................
-	nuevoVerticeParaZona( nombreZona, vertice, callback ) {
+	nuevoVerticeParaZona ( nombreZona, vertice, callback ) {
 
-		var textoSQL = 'insert into Vertice values ( $nombre, $longitud, $latitud );'
+		var textoSQL = 'insert into Vertice values ( $nombre, $longitud, $latitud );' ;
+
 		var datos = {
 			$nombre: nombreZona,
 			$longitud: vertice.longitud,
 			$latitud: vertice.latitud
-		}
+		};
 		
-		this.laConexion.modificarConPrepared( textoSQL, datos, callback )
+		this.laConexion.modificarConPrepared( textoSQL, datos, callback ) ;
 											  
 	} // ()
 
 	// .................................................................
+	//
+	// nombreZona: Texto
+	// -->
+	//    getZona() 
+	// -->
+	// zona: JSON <nombreZona: Texto, descripción: Texto, vértices: <lan, lng> > // vía callback (err, res)
+	//
 	// .................................................................
-	getZona( nombreZona, callback ) {
-	} // ()
+	getZona ( nombreZona, callback ) {
+
+		//consulta a la base de datos
+		var textoSQL = `select * from Zona where nombre = "${nombreZona}";` ;
+		
+		this.laConexion.consultar ( textoSQL, ( err, res ) => {
+
+			// miro si hay error
+			// y no sigo
+			if(err){
+				callback(err, res) ;
+				return;
+			}
+
+			// miro si hay resultado
+			// y no sigo
+			/*if(res){
+				callback(null, res) ;
+				return;
+			}*/
+
+			// mirar si el array está vacío
+			// y no sigo
+			if ( res.length == 0 ) {
+
+				// no hay error
+				// vaya, no hay zonas con ese nombre
+				// en caso de no haber resultado se devuelve una variable string informando de que no hay zonas con ese nombre
+				callback( null, "Oups, no hay zonas con ese nombre.") ;
+
+				return;
+			}
+
+			// error no hay, y si hay resultado
+			 callback(null, res) ;
+
+		}) ; // finalizada la consulta
+		
+	} // getZona()
+
+	// .................................................................
+	//
+	// nombreZona: Texto
+	// -->
+	//    getVertices() 
+	// -->
+	// vertices <lat:R, lng R>
+	//
+	// .................................................................
+	getVertices ( nombreZona, callback ) { 
+
+		//consulta a la base de datos
+		var textoSQL = `select * from Vertice where nombreZona = "${nombreZona}";` ;
+		
+		this.laConexion.consultar ( textoSQL, ( err, res ) => {
+
+			// mirar si hay error
+			// y no sigo
+			if( err ){
+				callback(err, null) ;
+				return;
+			}
+
+			// mirar si el array está vacío
+			// y no sigo
+			if ( res.length == 0 ){
+
+				callback( null, "Oups, no existen vértices para la zona indicada") ;
+
+				return;
+			}
+
+			var vertices = [] ; // creamos el array "vertices"
+
+			vertices.push ( res[0].longitud ) ;	
+
+			vertices.push ( res[0].latitud ) ;
+
+			callback ( null, vertices ) ;
+
+
+		}) ; // finalizada la consulta
+
+	} // getVertices()
+
+	// .................................................................
+	//
+	// nombreZona: Texto
+	// -->
+	//    getDescripcionDeZona() 
+	// -->
+	// descripción: texto // via callback(err, res)
+	//
+	// .................................................................
+	getDescripcionDeZona ( nombreZona, callback ) { 
+
+		//consulta a la base de datos
+		var textoSQL = `select * from Zona where nombre = "${nombreZona}";` ;
+		
+		this.laConexion.consultar ( textoSQL, ( err, res ) => {
+
+			// mirar si hay error
+			// y no sigo
+			if( err ){
+				callback(err, null) ;
+				return;
+			}
+
+			// mirar el array está vacío
+			// y no sigo
+			if ( res.length == 0 ){
+
+				callback( null, "Oups, no hay zonas con ese nombre.") ;
+
+				return;
+			}
+
+			// mirar si existe la zona pero no hay descripción
+			// y no sigo
+			if ( res.descripcion == 0 ){		//res.descripcion == null => NO PASA LOS TESTS
+
+				callback( null, "La zona seleccionada no tiene descripción");
+
+				return;
+			}
+
+			// Si no existe error, devolver el resultado
+			callback ( null, res[0].descripcion ) ;
+
+		}) ; // finalizada la consulta
+
+	} // getDescripcionDeZona()
 
 } // class
 
--- logica/test/mainTest1.js
+++ logica/test/mainTest1.js
@@ -6,9 +6,9 @@
 
 // --------------------------------------------------------
 // --------------------------------------------------------
-const Logica = require( "../Logica.js" )
+const Logica = require( "../Logica.js" ) ;
 
-var assert = require ('assert')
+var assert = require ('assert') ;
 
 // --------------------------------------------------------
 // main ()
@@ -17,45 +17,54 @@ var assert = require ('assert')
 //
 //
 //
-describe( "Test 1 (probar a añadir zonas)", function() {
+describe( "Test 1 (probar a añadir zonas)", () => {
 
 	// ....................................................
 	//
 	// ....................................................
-	var laLogica = null
-	before(  function( hecho ) {
-		laLogica = new Logica(
-			"../datos/Zonas.bd",
-			function( err ) {
-				assert.equal( null, err, " conexión a BD fallada: " + err)
-				hecho()
-			}) 
-	}) // before
+	var laLogica = null ;
+
+	// Esto nos asegurará que ocurra antes
+	// que cualquier otra cosa
+	before(  ( hecho ) => {
+
+		laLogica = new Logica("../datos/Zonas.bd", ( err ) => {
+
+				assert.equal( null, err, `conexión a BD fallada: ${err}`) ;
+
+				hecho() ;
+
+			});
+	}); // before
 
 	// ....................................................
 	//
 	// ....................................................
-	it( "borro datos ", function( hecho ){
+	it( "borro datos ", ( hecho ) => {
 
-		laLogica.borrarTodosLosDatos( function() {
-			hecho()
-		})
+		laLogica.borrarTodosLosDatos( () => {
+
+			hecho() ;
+
+		});
 		
-	}) // it
+	}); // it
 
 	// ....................................................
 	//
 	// ....................................................
-	it( "inserto zona marjal", function( hecho ){
+	it( "inserto zona marjal", ( hecho ) => {
 
 		var datos = {
 			nombre: "marjal",
 			descripcion: "marjal al lado del Grau de Gandia"
 		}
 
-		laLogica.nuevaZona( datos, function(err) {
-			assert.ok( ! err, " ¿hay error? : " + err )
-			hecho()
+		laLogica.nuevaZona( datos, (err) =>  {
+
+			assert.ok( ! err, `Existe un error: ${err}` ) ;
+			
+			hecho() ;
 		})
 		
 	}) // it
@@ -63,15 +72,15 @@ describe( "Test 1 (probar a añadir zonas)", function() {
 	// ....................................................
 	//
 	// ....................................................
-	it( "inserto un vertice para marjal", function( hecho ){
+	it( "inserto un vertice para marjal", ( hecho ) => {
 
 		var vertice = {
 			longitud: 0, // X
-			latitud: 1 // Y
+			latitud:  1// Y
 		}
 
 		laLogica.nuevoVerticeParaZona( "marjal", vertice, function( err ) {
-			assert.ok( ! err, "" + err )
+			assert.ok( ! err, `Existe un error: ` )
 
 			hecho()
 		})
@@ -81,25 +90,167 @@ describe( "Test 1 (probar a añadir zonas)", function() {
 	// ....................................................
 	//
 	// ....................................................
-	it( "pruebo getZonas()", function( hecho ){
+	it( "pruebo que getZona() con una zona existente", ( hecho ) => {
 
 		// llamo a getZona() y compruebo que me devuelve
 		// lo que antes he guardado
-		/*
-		  COMPLETAR
-		  laLogica.getZona( 
-		*/
 
-		assert.equal( true, false, " FALTA HACER ESTE TEST " )
+		laLogica.getZona ( "marjal", ( err, res) => {
 
-		hecho()
+			// no existe error
+			assert.ok(!err) ;
+
+			// compruebo que existe la zona "marjal"
+			assert.ok(res) ;
+			console.log(res) ;
+		}) ;
+
+		hecho() ;
 		
-	}) // it
+	}); // it
 
 	// ....................................................
 	//
 	// ....................................................
-	after( function() {
+	it( "pruebo getZona() con una zona NO existente", ( hecho ) => {
+
+		// llamo a getZona() y compruebo que me devuelve
+		// el mensaje de error
+		laLogica.getZona ( "zonaInventada", ( err, res) => {
+
+			// no existe error
+			assert.ok(!err) ;
+
+			// compruebo que devuelve el mensaje de error	
+			assert.equal(res, "Oups, no hay zonas con ese nombre.") ;
+			
+			assert.ok(res.includes("no hay")) ;
+
+		}) ;
+
+		hecho() ;
+		
+	}); // it
+
+	// ....................................................
+	//
+	// ....................................................
+
+	it("pruebo getVertices() con una zona existente", (hecho) => {
+
+		// llamo a getVertice() y compruebo que me devuelve
+		// los vértices de la zona marjal
+		laLogica.getVertices("marjal", ( err, res ) => {
+
+			// no existe error
+			assert.ok(!err, `Error: ${err}`) ;
+
+			// compruebo que devuelve un resultado
+			assert.ok(res) ;
+
+			console.log(`Vértices: ${res}`) ;
+
+			// compruebo que ese resultado que devuelve son los vértices
+			assert.equal( res[0], 0 ) ;
+			assert.equal( res[1], 1) ;
+
+		});
+
+		hecho();
+		
+		
+	}); // it
+
+	// ....................................................
+	//
+	// ....................................................
+	
+	it("pruebo getVertices() con una zona NO existente", (hecho) => {
+
+		// llamo a getVertice() y compruebo que me devuelve
+		// los vértices que le he guardado
+		laLogica.getVertices("zonaInventada", ( err, res ) => {
+
+			// no existe error
+			assert.ok(!err, `Error: ${err}`) ;
+
+			// compruebo que devuelve un resultado
+			assert.ok(res) ;
+
+			// compruebo que ese resultado que devuelve es el mensaje de error
+			assert.equal( res, "Oups, no existen vértices para la zona indicada" ) ;
+
+			assert.ok ( res.includes("no existen") ) ;
+
+		});
+
+		hecho();
+		
+		
+	}); // it
+
+	// ....................................................
+	//
+	// ....................................................
+
+	it("pruebo getDescripcionDeZona() con una zona existente", (hecho) => {
+
+		// llamo a getDescripcionDezona() y compruebo que me devuelve
+		// la descripción de la zona marjal
+		laLogica.getDescripcionDeZona("marjal", ( err, res ) => {
+
+			// no existe error
+			assert.ok(!err, `Error: ${err}`) ;
+
+			// compruebo que devuelve un resultado
+			assert.ok(res) ;
+
+			// compruebo que ese resultado que devuelve es la descripción
+			assert.equal( res, "marjal al lado del Grau de Gandia" ) ;
+
+			assert.ok ( res.includes("al lado del Grau") ) ;
+
+		});
+
+		hecho();
+		
+		
+	}); // it
+
+		// ....................................................
+	//
+	// ....................................................
+
+	it("pruebo getDescripcionDeZona() con una zona NO existente", (hecho) => {
+
+		// llamo a getDescripcionDeZona() y compruebo que me devuelve
+		// el mensaje de error
+		laLogica.getDescripcionDeZona("zonaInventada", ( err, res ) => {
+
+			// no existe error
+			assert.ok(!err, `Error: ${err}`) ;
+
+			// compruebo que devuelve un resultado
+			assert.ok(res) ;
+
+			console.log(res)
+
+			// compruebo que ese resultado que devuelve es el mensaje de error
+			assert.equal( res, "Oups, no hay zonas con ese nombre." ) ;
+
+			assert.ok ( res.includes("Oups") ) ;
+
+		});
+
+		hecho();
+		
+		
+	}); // it
+
+	// ....................................................
+	//
+	// ....................................................
+	after( () =>{
 		laLogica.cerrar()
 	})
 	
