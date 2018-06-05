ac6164bc53d5e84c3aa1f0ba747d41f7975234c4
osblasae <osblasae@epsg.upv.es>
2018-06-05 11:47:48 +0200

    Todo listo para empezar la tercera práctica (Login)

--- servidorRest/ReglasREST.js
+++ servidorRest/ReglasREST.js
@@ -4,12 +4,211 @@
 // 
 // ------------------------------------------------------
 
+// .......................................................
+//
+// user: Texto
+// password: Texto
+// -->
+//    f()
+// -->
+// Verdadero / Falso
+// JSON datos de usuario {email, rol, nombre, apellidos}
+//
+// via callback( error, resultados )
+//
+// ..................................................
+// Esta función es llamada por la regla GET /login
+// .......................................................
+function comprobarLoginEnBD (user, password, callback) {
+	// Comprobar login no es inmediato sino 
+	// asíncrono ( por eso ahora lo simulo ).
+	// En realidad aquío se debe consultar en
+	// una BD el nombre del usuario y
+	// su password (secreto compartido)
+	// 
+	// user == email de usuario (utilizado como id/clave)
+	// user no es el nombre de pila del usuario
+	setTimeout ( () => {
+
+			if ( 2 == 1 + 1 ) { // buena comprobación 
+				// devuelvo que no hay error y
+				// unos datos de usuario inventados
+
+				callback (null,
+					{
+							  user: user,
+							  email: user,
+							  rol: "jefazo",
+							  nombre: "Pepe",
+							  apellidos: "El Grande"
+						  });
+			}
+		},
+		100)
+} // ()
+
 // ------------------------------------------------------
 // ------------------------------------------------------
 // reglas de peticiones REST
 // ------------------------------------------------------
 // ------------------------------------------------------
 module.exports.cargar = ( servidorExpress, laLogica ) => {
+
+ 	// .......................................................
+	// .......................................................
+	//
+	// ACREDITACION DEL USUARIO (LOGIN)
+	// GET /login?user=xxx&password=yyy
+	//
+	// (por que es así como envía de forma automática
+	// los form de html con method=get, el method=post
+	// pone user=xxx&password=yyy en el body)
+	//
+	// .......................................................
+	// .......................................................
+	servidorExpress.get('/login', (req, response) => {
+
+		console.log( "\n --------------------------------------- ")
+		console.log( " * GET /login      " + req.url )
+		console.log( " --------------------------------------- ")
+
+		//
+		// 
+		//
+		var user = req.query.user ;
+
+		var password = req.query.password ;
+
+		console.log (`\t user = ${user}`)
+		console.log (`\t password= ${password}`)
+
+		var laSesion = req.session ;
+
+		console.log (`\t sesion=  ${JSON.stringify(laSesion)} `)
+
+		// 
+		// ahora comprobamos usuario y password
+		// 
+		comprobarLoginEnBD( user, password, ( err, datosUsuario ) => {
+
+			if ( ! err ) {
+
+				// 
+				//  respuesta positiva
+				// 
+				console.log ("          ** LOGIN OK **")
+
+				// 
+				//  si login es correcto, hay que generar un
+				// "token opaco" (codificado con una clave sólo
+				//  conocida aquí) y ponerlo en las sesion (cookie)
+				//  para que apartir de ahora nos lo vuelva a
+				//  enviar en las futuras peticiones
+				//  En ese token opaco hay que añadir el nombre
+				//  del usuario acreditado, y su secreto compartido
+				// 
+				laSesion.elTokenOpaco = JSON.stringify(
+					{
+						autor: "jordi", // para hacer una comprobacion simple
+						comentario: "Esto es un token opaco puesto en GET /login",
+						user: user,
+						password: password,
+						rol: datosUsuario.rol
+					});
+
+				// 
+				// Aunque esté en el token opaco,
+				// también podemos poner el nombre del usuario en la sesion
+				// para recordarlo más fácilente
+				// 
+				laSesion.usuario = user ;
+
+				// 
+				// 
+				// 
+				response.writeHead(200, {'Content-Type': 'text/json'}) ;
+
+				// 
+				// damos los  datos del usuario en JSON en el body
+				// 
+				response.write( JSON.stringify( datosUsuario ) ) ;
+
+				response.end;
+
+			} else {
+				// ok == false
+				console.log ("          ERROR EN LOGIN")
+
+				// 
+				// 
+				// 
+				response.writeHead(401) ; // unauthorized
+				response.end;
+
+			}
+			
+		}) // comprobarLoginEnBD 
+
+	}) // GET /login
+
+	// .......................................................
+	// .......................................................
+	//  GET /prueba
+	// .......................................................
+	// .......................................................
+	servidorExpress.get('/prueba', (req, res) => {
+
+		console.log( "\n --------------------------------------- ")
+		console.log( " * GET /prueba " )
+		console.log( `\t url=" ${req.url} `)
+		console.log( " --------------------------------------- ")
+
+		//
+		//
+		//
+		var laSesion = req.session ;
+
+		console.log ( `   usuario  que me pide (antes acreditado) = " + ${laSesion.usuario}` ) ;
+		
+		/* si quisiera cambiar o añadir cosas a la sesion
+		   laSesion.elTokenOpaco = "esto es un token opaco puesto en GET /prueba"
+		   laSesion.elTokenOpaco = "esto es un token opaco puesto en GET /prueba"
+		   laSesion.usuario = laSesion.usuario + "x"
+		*/
+
+		//
+		//
+		//
+		//res.writeHead(200, {'Content-Type': 'text/plain'})
+		res.writeHead(200, {'Content-Type': 'text/json'})
+		
+		res.write ( '{"hola": "mundo"}' )
+		
+		res.end()
+
+	})
+
+	// .......................................................
+	// .......................................................
+	//  GET /
+	// como prueba por si alguien pregunta
+	// .......................................................
+	// .......................................................
+	servidorExpress.get('/', (req, res) => {
+
+		console.log (" * GET /" )
+		console.log (`\t url= ${req.url} `)
+
+		//
+		//
+		//
+		res.writeHead(200, {'Content-Type': 'text/plain'})
+		
+		res.write ( "GET / dice: hola mundo ")
+		
+		res.end;
+
+	})
 
 	// .......................................................
 	// .......................................................
--- servidorRest/mainServidorREST.js
+++ servidorRest/mainServidorREST.js
@@ -16,15 +16,14 @@ const assert = require( 'assert' ) ;
 
 const Logica = require( "../logica/Logica.js" ) ;
 
+var expressSession = require('express-session')  ;
+
 
 // .......................................................
 // creación del servidor 
 // .......................................................
 var servidorExpress = express() ;
-
-// .......................................................
-// servidorExpress.use ( _ ) pone un "hook"
-// 
+ 
 // servidorExpress.use (bodyParser())
 // Esto es para copiar lo que haya en el cuerpo
 // de la peticion HTTP al cambo "body"  de "req"
@@ -79,7 +78,231 @@ laLogica = new Logica("../datos/Zonas.bd", ( err ) => {
 		});
 
 		
-    }); // laLogica
+	}); // laLogica
+	
+	// .......................................................
+// servidorExpress.use ( _ ) pone un "hook"
+// para usar sesiones
+// .......................................................
+servidorExpress.use ( expressSession(	{
+		secret: "soy un secreto",
+
+		//
+		// OJITO con la duración de los cookies (maxAge).
+		// Si la ponemos muy corta o el reloj
+		// de alguna máquina no está bien,
+		// el navegador/cliente rest
+		// pueda que no la reenvie porque // la ve caducada.
+		// Entonces aquí no llega y la verificación falla
+		//
+		// cookie: { maxAge: 1000*60*8 n}, // 8 minutos
+		cookie: { maxAge: 1000*60*60*24 }, // 1 día
+
+		resave: true,
+		saveUninitialized: true
+	}
+) )
+
+// --------------------------------------------------------
+// funciones de utilidad relacionadas con autentificación y autorización:
+// --------------------------------------------------------
+
+// .......................................................
+//
+// req: PeticionHTTP
+// -->
+//    f()
+// -->
+// V/F
+//
+// .......................................................
+function peticionEsGetLogin (req) {
+
+	// console.log (" peticionEsGetLogin() valorando: ", req.method, req.path, req.url)
+
+ 	if (req.path != "/login") {
+
+		return false;
+	}
+
+	if ( req.method != "GET" ) {
+		return false;
+	}
+
+	return true;
+} // ()
+
+// .......................................................
+//
+// req: PeticionHTTP
+// -->
+//    f()
+// -->
+// V/F
+//
+// .......................................................
+function peticionTienePermisos( req ) {
+
+	console.log (" * comprobando permisos ") ;
+
+	// 
+	// sacamos el token opaco de la sesion (viaja como cookie)
+	// 
+	var laSesion = req.session ;
+
+	// console.log ("\t sesion=" + JSON.stringify(laSesion) )
+	console.log ("\t token opaco que viaja como cookie=" + laSesion.elTokenOpaco ) ;
+
+	//
+	// el token opaco debería haber sido encriptado
+	// aquí se desencriptaría antes del parse
+	//
+	try {
+
+		var elToken = JSON.parse( laSesion.elTokenOpaco ) ;
+
+	} catch ( err ) {
+
+		console.log(" * el token no parece auténtico: :-( Permiso no concedido") ;
+
+		return false;
+
+	}
+
+	//
+	// Como es una prueba, ahora, compruebo algo simple
+	//
+	if ( elToken.autor != "jordi" )  {
+
+		console.log(" * el token no parece auténtico: :-( Permiso no concedido");
+
+		return false;
+	}
+	
+	console.log (" el token parece bueno: permisos OK") ;
+	return true;
+
+	/*
+	  La comprobación con el token opaco sería: 
+	  el programa cliente sabe el nombre de usuario 'juan' p.ej y el secreto compartido
+	  secreto compartido <- 1234('juan') // 1234 solo lo sabe el ser humano
+	  y no lo guarda ninguna máquina
+	  secreto compartido también están en la base de datos como password de juan
+	  (por eso es compartido)
+	  El secrto cmpartido se pone en el token opaco (es opaco porque va codificado
+	  con una clave conocida sólo aquí), también se pone el nombre del usuario.
+
+	  Para probar su identidad, el programa cliente envia el token opaco 
+	  y el nombre del usuario codificado con el secreto compartido
+
+	  Aquí, abriríamos el token opaco para sacar el secreto comnpartido
+	  y con el decodificar la prueba que debe dar el nombre del usuario
+	 */
+} // ()
+
+// .......................................................
+// servidorExpress.use ( _ ) pone un "hook"
+// 
+// Este hook hace que en la respuesta
+// esté siempre el campo de cabecera:
+// Access-Control-Allow-Origin
+// con el correspondiente valor
+// 
+// Lo necesito porque tengo páginas html servidas
+// en localhost:8080 por un servidor python (en todo caso,
+// diferente de éste)
+// y el código html de esas páginas hace peticiones REST a
+// aquí (que lo sirve este servidor node.js en localhost:8081)
+// 
+// De esta forma, el navegador acepta que una página
+// cargada desde 8080 haga preguntas a aquí 8081
+// se llama CORS  (cross origin resource sharing)
+//
+// Igualmente, Allow-Credentials para que el navegador
+// guarde y re-envie los cookies de forma automática
+// ya que el código javascript de la págna html tiene
+// prohibido el acceso a las cookies
+//
+// .......................................................
+servidorExpress.use ( (req, res, next) => {
+
+	res.setHeader ('Access-Control-Allow-Origin', "http://localhost:8080") ;
+
+	res.setHeader ('Access-Control-Allow-Credentials', "true" ) ;
+
+	// res.setHeader ('Access-Control-Allow-Methods', "put" ) 
+
+	next () ;
+
+})
+
+// .......................................................
+// servidorExpress.use ( _ ) pone un "hook"
+// 
+// Esto es una "interceptación" ANTES
+// de llamar a las reglas de REST:
+// inserto código para comprobar que la petición
+// si no es de login, tiene permisos.
+// Tener permisos significa que la petición
+// en una cookie llava un token opaco
+// que se le entrega al cliente tras un login correcto.
+// para que lo reenvíe siempre.
+// 
+// Si la petición es de login, la dejo pasar
+// hasta la regla correspondiente GET /login
+// para que allí se compruebe usuario-password
+// .......................................................
+servidorExpress.use ( (req, res, next) => {
+
+	console.log ("\n -------------------------------------------")
+	console.log (" -- hook antes de la reglas inspecciona peticion ----")
+	console.log ("\t method = " + req.method )
+	console.log ("\t url    =" + req.url )
+	console.log (" --------------------------------------------- ")
+
+	// 
+	// si la petición es de login, la dejo pasar
+	// 
+	if ( peticionEsGetLogin (req) ) {
+
+		console.log (" * es login ") ;
+
+		next () ; // la peticion continua
+
+		return;
+	}
+
+	// 
+	// 
+	// 
+	console.log (" * no es login: debo comprobar si tiene permisos ") ;
+
+	// 
+	// Como no es login voy a mirar si tiene permisos.
+	// Lo hace la función de utilidad inspeccionando
+	// el token opaco que viaja como cookie en la peición.
+	// Una ventaja de esto es que la comprobración es síncrona
+	// porque lo que hay que mirar (el token opaco en la cookie)
+	// ya lo tenemos (y no hace falta hacer consultar en la BD, pj.)
+	// 
+	if (! peticionTienePermisos (req) ){ 
+
+		console.log (" * no tiene permisos ") ;
+
+		res.writeHead(401) ; // unauthorized
+
+		res.end;
+
+		return; // acabo sin llamar a next()
+	}
+
+	// 
+	// 
+	// 
+	console.log (" * sí que tiene permisos ") ;
+
+    next(); // la petición contiúa
+})
 
 // ------------------------------------------------------
 // ------------------------------------------------------
